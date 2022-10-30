243a0743cf2308737bc90bf3ee25545c11cbb993
alexey.minay <alexey.minay@ispring.com>
2022-10-30 15:11:30 +0300

    added try/catch

--- app/src/main/java/com/alexey/minay/downloader/data/FileDownloader.kt
+++ app/src/main/java/com/alexey/minay/downloader/data/FileDownloader.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.isActive
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.File
+import java.lang.Exception
 import java.net.HttpURLConnection
 
 class FileDownloader() {
@@ -14,39 +15,43 @@ class FileDownloader() {
         .build()
 
     fun download(url: String, file: File) = flow {
-        val request = Request.Builder().url(url)
-            .build()
-        val response = mOkHttpClient.newCall(request).execute()
-        val body = response.body()
-        val responseCode = response.code()
-        if (responseCode >= HttpURLConnection.HTTP_OK &&
-            responseCode < HttpURLConnection.HTTP_MULT_CHOICE &&
-            body != null
-        ) {
-            val length = body.contentLength()
-            if (length <= 0) {
+        try {
+            val request = Request.Builder().url(url)
+                .build()
+            val response = mOkHttpClient.newCall(request).execute()
+            val body = response.body()
+            val responseCode = response.code()
+            if (responseCode >= HttpURLConnection.HTTP_OK &&
+                responseCode < HttpURLConnection.HTTP_MULT_CHOICE &&
+                body != null
+            ) {
+                val length = body.contentLength()
+                if (length <= 0) {
+                    emit(Result.Error)
+                    return@flow
+                }
+                val byteStream = body.byteStream()
+
+                file.outputStream().use { fileOut ->
+                    var bytesCopied = 0L
+                    val buffer = ByteArray(DEFAULT_BUFFER_SIZE * 10)
+                    var bytes = byteStream.read(buffer)
+                    while (bytes >= 0 && currentCoroutineContext().isActive) {
+                        fileOut.write(buffer, 0, bytes)
+                        bytesCopied += bytes
+                        bytes = byteStream.read(buffer)
+                        emit(Result.Progress(progress = bytesCopied, total = length))
+                    }
+                }
+            } else {
                 emit(Result.Error)
                 return@flow
             }
-            val byteStream = body.byteStream()
 
-            file.outputStream().use { fileOut ->
-                var bytesCopied = 0L
-                val buffer = ByteArray(DEFAULT_BUFFER_SIZE * 5)
-                var bytes = byteStream.read(buffer, 0, DEFAULT_BUFFER_SIZE * 5)
-                while (bytes >= 0 && currentCoroutineContext().isActive) {
-                    fileOut.write(buffer, 0, bytes)
-                    bytesCopied += bytes
-                    bytes = byteStream.read(buffer)
-                    emit(Result.Progress(progress = bytesCopied, total = length))
-                }
-            }
-        } else {
+            emit(Result.Success)
+        } catch (e: Exception) {
             emit(Result.Error)
-            return@flow
         }
-
-        emit(Result.Success)
     }
 
 }
